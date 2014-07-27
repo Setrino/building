af1523a92b63da745c72bf606d422e9ff0752b46
Setrino <sk@fkdn-lab.com>
2014-07-27 21:49:01 +0200

    Setting dev environement

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res){
+   res.render('index',{
+       title: 'Main',
+       supplies: ['mop', 'broom', 'duster']
+   });
+});
+
+router.get('/header', function(req, res){
+   res.render('header', {
+       main_text: 'NA, Testing'
+   })
 });
 
 module.exports = router;
